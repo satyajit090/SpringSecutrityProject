b4f868b471be2583ea58eee59602d7e339048486
satyajit090 <beherasatyajit090@gmail.com>
2026-04-05 22:27:15 +0530

    Initial commit

---
+++ src/main/java/com/satyajit/SpringSecutrityProject/SpringSecutrityProjectApplication.java
@@ -0,0 +1,13 @@
+package com.satyajit.SpringSecutrityProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecutrityProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecutrityProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/satyajit/SpringSecutrityProject/SpringSecutrityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satyajit.SpringSecutrityProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecutrityProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
